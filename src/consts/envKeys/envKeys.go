020992c80bd30b025c760fd9c175218e360e2dd3
virtouso <s_moeen_kamali@yahoo.com>
2025-09-20 04:10:36 +0330

    enc consts

---
+++ src/consts/envKeys/envKeys.go
@@ -0,0 +1,6 @@
+package envKeys
+
+const RedisAddress = "redis_address"
+const RemoveOldRecordsInterval = "remove_old_records_interval"
+const KeepAliveMinutes = "keep_alive_minutes"
+const Port = "port"
